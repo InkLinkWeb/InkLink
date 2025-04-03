94ec79c291e4dc8a4f6c46451f8ca81fa4be8cd8
Sebastian Barahona-Gonzalez <sbarahon@syr.edu>
2025-04-03 02:37:05 -0400

    changing nav

--- scripts/navbarHandler.js
+++ scripts/navbarHandler.js
@@ -1,4 +1,5 @@
 import { logout } from '/InkLink/scripts/AccountHandler.js';
+
 // Ensure the script runs after the DOM is fully loaded
 $(document).ready(function() {
     // Load the navbar HTML into the #navbar-container div
@@ -10,37 +11,31 @@ $(document).ready(function() {
         // Once the navbar is loaded, initialize its functionalities
         initializeNavbar();
     });
-
-    // Dynamically load the navbar CSS
-    var navbarCSS = $('<link>', {
-        rel: 'stylesheet',
-        type: 'text/css',
-        href: '/InkLink/scripts/navbar.css'
-    });
-    $('head').append(navbarCSS);
 });
 
 // Function to initialize navbar functionalities
 function initializeNavbar() {
     // Highlight the active navigation link
-    var currentPath = window.location.pathname.split('/').pop();
-    $('.nav-link').each(function() {
-        var linkPath = $(this).attr('href').split('/').pop();
-        if (currentPath === linkPath) {
-            $(this).addClass('bg-[#5CB8E4] text-white');
-        } else {
-            $(this).addClass('text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium');
-        }
-    });
+    // var currentPath = window.location.pathname.split('/').pop();
+    // $('.nav-link').each(function() {
+    //     var linkPath = $(this).attr('href').split('/').pop();
+    //     if (currentPath === linkPath) {
+    //         $(this).addClass('bg-[#5CB8E4] text-white');
+    //     } else {
+    //         $(this).addClass('text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium');
+    //     }
+    // });
 
     // Dropdown menu functionality
     var userMenuButton = $('#user-menu-button');
     var userMenu = $('#user-menu');
 
+    // Toggle the dropdown menu when the button is clicked
     userMenuButton.on('click', function() {
         userMenu.toggleClass('hidden');
     });
 
+    // Close the dropdown if the user clicks outside
     $(window).on('click', function(event) {
         if (!userMenuButton.is(event.target) && userMenuButton.has(event.target).length === 0 &&
             !userMenu.is(event.target) && userMenu.has(event.target).length === 0) {
