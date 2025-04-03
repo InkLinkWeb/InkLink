5fece3cde4f4e0620b34285c20ff136d1ee575f3
Sebastian Barahona-Gonzalez <sbarahon@syr.edu>
2025-04-02 20:53:09 -0400

    Update ImageHandler.js

--- scripts/ImageHandler.js
+++ scripts/ImageHandler.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getFirestore(app, 'gallerydata');
 console.log('Firestore initialized:', db);
 
 // Function to upload an image
